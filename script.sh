6fb9db9a585900e71ce80190c7e1337b0c5f9369
Martin Kustermann <kustermann@google.com>
2017-03-16 17:49:59 +0100

    t4

--- script.sh
+++ script.sh
@@ -6,9 +6,6 @@ export PUB_HOSTED_URL="https://20170316t153535-dot-dartlang-pub.appspot.com"
 export PUB_CACHE=/tmp/foo-$(date '+%Y-%m-%d__%H:%M:%S')
 rm -rf $PUB_CACHE
 
-curl -v "$PUB_HOSTED_URL/api/packages/appengine"
-
-cd app
 for i in $(seq 1 10); do
   curl -v "$PUB_HOSTED_URL/api/packages/appengine"
 done
