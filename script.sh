9da155fe896fa958c899bbe740d7b95a841bd975
Martin Kustermann <kustermann@google.com>
2017-03-16 18:43:57 +0100

    one instance: t2 - 1000

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ set -x
 export PUB_HOSTED_URL="https://20170316t153535-dot-dartlang-pub.appspot.com"
 export PUB_HOSTED_URL="https://20170316t182913-dot-dartlang-pub.appspot.com"
 
-for i in $(seq 1 100); do
+for i in $(seq 1 1000); do
   timestamp="$(date '+%Y_%m_%d__%H_%M_%S')"
   curl -v "$PUB_HOSTED_URL/api/packages/appengine?timestamp=$timestamp&iteration=$i" > /dev/null
 done
