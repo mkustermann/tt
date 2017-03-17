93d460505bdf9b519dded9db328e7f9029caf297
Martin Kustermann <kustermann@google.com>
2017-03-17 16:31:59 +0100

    one instance: t7 - 1000

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-export PUB_HOSTED_URL="https://20170316t231228-dot-dartlang-pub.appspot.com"
+export PUB_HOSTED_URL="https://20170317t162249-dot-dartlang-pub.appspot.com"
 export PUB_HOSTED_URL="https://pub.dartlang.org"
 
 for i in $(seq 1 1000); do
